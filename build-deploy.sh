a825e37e784c2841eb04af7e3478d15b457c23c5
ianad <ianad@duck.com>
2025-07-24 00:24:00 -0400

    Deploying latest changes

--- build-deploy.sh
+++ build-deploy.sh
@@ -3,8 +3,8 @@
 chmod +x build.sh deploy.sh
 
 ./build.sh
-echo "Opening the built book in the browser."
-xdg-open file:///home/ianad/repo/book/_build/html/index.html
+# echo "Opening the built book in the browser."
+# xdg-open file:///home/ianad/repo/book/_build/html/index.html
 
 ./deploy.sh
 echo "Waiting a moment for the deployment to kick off..."
