74b9dbf865c1c2ec3b397826b2bfcc32af20967b
ianad <ianad@duck.com>
2025-07-24 00:14:04 -0400

    Deploying latest changes

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ git push origin main
 ghp-import -c cognician.dev -n -p -f _build/html
 
 sleep 1
+echo "Waiting a moment for the deployment to kick off..."
 
+# Open the GitHub Actions page for the repository
 xdg-open "$(git remote get-url origin | sed -E 's#(git@|https://)([^:/]+)[:/]([^/]+)/([^.]+)(\.git)?#https://\2/\3/\4/actions#')"
 # gh repo view --branch gh-pages --web
