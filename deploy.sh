871c747d9a4c8b2f63d74d197e2247ed54efe2f0
ianad <ianad@duck.com>
2025-07-24 00:04:54 -0400

    Deploying latest changes

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ ghp-import -c cognician.dev -n -p -f _build/html
 sleep 1
 
 xdg-open "$(git remote get-url origin | sed -E 's#(git@|https://)([^:/]+)[:/]([^/]+)/([^.]+)(\.git)?#https://\2/\3/\4/actions#')"
-# gh repo view --web
+# gh repo view --branch gh-pages --web
