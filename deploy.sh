f83097411cc80c3555564b8435becbd23dbee745
ianad <ianad@duck.com>
2025-07-23 23:59:37 -0400

    Deploying latest changes

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ git commit -m "Deploying latest changes"
 git push origin main
 
 ghp-import -c cognician.dev -n -p -f _build/html
+
+xdg-open "$(git remote get-url origin | sed -E 's#(git@|https://)([^:/]+)[:/]([^/]+)/([^.]+)(\.git)?#https://\2/\3/\4/actions#')"
