1d9afa3fdf7d034ad8551b590e85b373c579edcb
ianad <ianad@duck.com>
2025-07-25 19:05:24 -0400

    Deploying latest changes

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ git push origin main
 
 ghp-import -c cognician.dev -n -p -f _build/html
 
-echo -n "Waiting for the new deployment to kick off"
-for i in {1..5}; do
-  sleep 1
-  echo -n "."
-done
+# echo -n "Waiting for the new deployment to kick off"
+# for i in {1..5}; do
+#   sleep 1
+#   echo -n "."
+# done
 
-echo -e "\nOpening the latest workflow in the browser."
-gh run list --branch gh-pages --limit 1 --json url --jq '.[0].url' | xargs xdg-open
+# echo -e "\nOpening the latest workflow in the browser."
+# gh run list --branch gh-pages --limit 1 --json url --jq '.[0].url' | xargs xdg-open
